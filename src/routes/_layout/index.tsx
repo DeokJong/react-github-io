7f654e658893c791ef9c820fa51e570aef46af4b
DeokJong <novelss444@jbnu.ac.kr>
2024-09-10 15:47:22 +0900

    etc: 기타 디자인 및 모듈 추가

--- src/routes/_layout/index.tsx
+++ src/routes/_layout/index.tsx
@@ -2,7 +2,9 @@ import { createFileRoute } from '@tanstack/react-router'
 import { Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 
-import { Article, Buttons, IndexButton } from '@/styles/index'
+import { ArticleColunm, ArticleRow, Buttons, IndexButton } from '@/styles/index'
+import ImageBox from '@/components/common/ImageBox'
+import { dockerLogo, reactLogo, viteLogo } from '@/assets/logo'
 
 export const Route = createFileRoute('/_layout/')({
   component: Index, // 컴포넌트 이름은 대문자로 시작
@@ -29,7 +31,7 @@ function Index() {
 
   return (
     <>
-      <Article>
+      <ArticleColunm>
         <Typography variant="h1" component="h1" gutterBottom>
           {t('index.name')}
         </Typography>
@@ -39,7 +41,7 @@ function Index() {
         <Typography variant="h6" color="textSecondary">
           {t('index.subtitle2')}
         </Typography>
-      </Article>
+      </ArticleColunm>
       <Buttons>
         {links.map((link) => (
           <IndexButton key={link.name} href={link.url} size="large">
@@ -47,6 +49,18 @@ function Index() {
           </IndexButton>
         ))}
       </Buttons>
+      <ArticleColunm>
+        <Typography variant="h4" component="h2" gutterBottom>
+          {t('index.skills')}
+        </Typography>
+        <ArticleRow>
+          <ImageBox svg={dockerLogo} />
+          <ImageBox svg={reactLogo} />
+          <ImageBox svg={viteLogo} />
+        </ArticleRow>
+      </ArticleColunm>
+      <ArticleColunm>MY University</ArticleColunm>
+      <ArticleColunm>I&apos;m learning Skills...</ArticleColunm>
     </>
   )
 }
--- src/styles/common/article.styles.ts
+++ src/styles/common/article.styles.ts
@@ -1,11 +1,18 @@
 import { Box, styled } from '@mui/material'
 
-export const Article = styled(Box)`
+export const ArticleColunm = styled(Box)`
   margin: 2rem;
   padding: 1rem;
   width: 100%;
   display: flex;
   justify-content: center;
   flex-direction: column;
+  align-items: center;
+`
+
+export const ArticleRow = styled(Box)`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
   align-items: center;
 `
