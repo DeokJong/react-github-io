42473d776af431a9ca440a85470b4a26ebec9bde
DeokJong <novelss444@jbnu.ac.kr>
2024-09-10 09:42:34 +0900

    add: vite-tsconfig-paths 라이브러리 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
-import { resolve } from 'path'
-
+import react from '@vitejs/plugin-react-swc'
 import eslint from 'vite-plugin-eslint'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import viteReact from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact(), eslint()],
-  base: './',
-  resolve: {
-    alias: {
-      '@assets': resolve(__dirname, 'src/assets'),
-      '@': resolve(__dirname, 'src'),
-      '@atoms': resolve(__dirname, 'src/atoms'),
-      '@styles': resolve(__dirname, 'src/styles'),
-    },
-  },
+  plugins: [tsconfigPaths(), TanStackRouterVite(), react(), eslint()]
 })
